4d8b42182a03cfaa60f9164c095f7c3319e93200
Hexilee <hexileee@gmail.com>
2019-06-25 15:24:18 +0800

    add Future, Poll and Context

--- future/future.cpp
+++ future/future.cpp
@@ -3,3 +3,20 @@
  */
 
 #include "future.hpp"
+
+template<class Output>
+auto Poll<Output>::pending() -> Poll {
+    Poll poll;
+    poll.complete = false;
+    return poll;
+}
+
+template<class Output>
+auto Poll<Output>::is_complete() -> bool {
+    return complete;
+}
+
+template<class Output>
+auto Poll<Output>::get() -> Output {
+    return value;
+}
--- future/future.hpp
+++ future/future.hpp
@@ -6,9 +6,25 @@
 #define SILVER_IO_FUTURE_HPP
 
 template<class Output>
+class Poll {
+    Output value;
+    bool complete;
+  public:
+    explicit Poll(const Output value) : Output(value), complete(true) {};
+    static auto pending() -> Poll;
+    auto is_complete() -> bool;
+    auto get() -> Output;
+};
+
+class Context {
+  public:
+    virtual auto wake() -> void;
+};
+
+template<class Output>
 class Future {
   public:
-    virtual auto poll() -> Output;
+    virtual auto poll(const Context &ctx) -> Poll<Output>;
 };
 
 #endif //SILVER_IO_FUTURE_HPP
