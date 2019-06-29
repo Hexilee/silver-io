3a7eff6e1ef64fb41e5919c196dbc49788da1b6c
Hexilee <hexileee@gmail.com>
2019-06-29 22:05:26 +0800

    bug: cannot find errno in __err

---
+++ coroutine/async.cpp
@@ -0,0 +1,5 @@
+/*
+ * Created by 李晨曦 on 2019-06-29.
+ */
+
+#include "async.hpp"
---
+++ coroutine/async.hpp
@@ -0,0 +1,11 @@
+/*
+ * Created by 李晨曦 on 2019-06-29.
+ */
+
+#ifndef SILVER_IO_ASYNC_HPP
+#define SILVER_IO_ASYNC_HPP
+#include <__errc>
+
+auto a = EADDRINUSE;
+
+#endif //SILVER_IO_ASYNC_HPP
