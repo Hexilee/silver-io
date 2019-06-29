69cc6959623b24c3fc65b9506e08d99a6074b3c7
Hexilee <hexileee@gmail.com>
2019-06-30 00:45:43 +0800

    add macro await

--- coroutine/async.hpp
+++ coroutine/async.hpp
@@ -5,7 +5,16 @@
 #ifndef SILVER_IO_ASYNC_HPP
 #define SILVER_IO_ASYNC_HPP
 #include <thread>
+#define await(future_obj) ({\
+    auto result = future_obj.poll(sio::ThreadLocalContext);\
+    while (!result.is_complete()) {\
+        yield(coroutine::Yield());\
+        result = future_obj.poll(sio::ThreadLocalContext);\
+    }\
+    result.get()\
+})
+
 namespace coroutine {
-    using std::thread;
+    struct Yield {};
 }
 #endif //SILVER_IO_ASYNC_HPP
