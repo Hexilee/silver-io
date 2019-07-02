0b0b2f51eed59f24d288170affad15522e36a18e
Hexilee <hexileee@gmail.com>
2019-07-03 01:50:35 +0800

    StreamIter still segmentation fault

--- coroutine/test/stream_iter.cpp
+++ coroutine/test/stream_iter.cpp
@@ -4,15 +4,29 @@
 
 #include "gtest/gtest.h"
 #include "coroutine/stream_iter.hpp"
+#include "coroutine/async.hpp"
 #include "future/stream.hpp"
 
 using namespace sio::future;
 using namespace sio::coroutine;
 
 TEST(RangeStreamTest, StreamIter) {
-    auto stream = RangeStream<0, 20, 2>();
-    auto iter = StreamIter(std::move(stream));
-    for (auto i: iter) {
-        std::cout << i << std::endl;
+    auto iter_future = async [] {
+        auto stream = RangeStream<0, 20, 2>();
+        auto iter = StreamIter(std::move(stream));
+        auto ret = 0;
+        for (auto i: iter) {
+            ret++;
+            std::cout << i << std::endl;
+        }
+        return ret;
+    };
+    uint64_t counter = 0;
+    auto poll_result = iter_future->poll();
+    while (poll_result.status() == PollStatus::Pending) {
+        counter++;
+        poll_result = iter_future->poll();
     }
+    std::cout << "counter = " << counter << std::endl;
+    std::cout << "poll_result = " << poll_result.get() << std::endl;
 }
