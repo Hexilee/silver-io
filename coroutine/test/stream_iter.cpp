7256407aca02b36dfe23d5ae27980f306a5ef884
Hexilee <hexileee@gmail.com>
2019-07-03 17:23:23 +0800

    RangeStreamTest pass

--- coroutine/test/stream_iter.cpp
+++ coroutine/test/stream_iter.cpp
@@ -10,23 +10,31 @@
 using namespace sio::future;
 using namespace sio::coroutine;
 
-TEST(RangeStreamTest, StreamIter) {
+template<int64_t From, int64_t To, int64_t Diff = 1>
+auto rang_stream_to_iter() {
     auto iter_future = async [] {
-        auto stream = RangeStream<0, 20, 2>();
+        auto stream = RangeStream<From, To, Diff>();
         auto iter = StreamIter(std::move(stream));
         auto ret = 0;
-        for (auto i = iter.begin(); i != iter.end(); i++) {
+        for (auto i: iter) {
+            EXPECT_EQ(ret * Diff + From, i);
             ret++;
-            std::cout << "i = " << *i << std::endl;
         }
         return ret;
     };
-    uint64_t counter = 0;
+    auto counter = 0;
     auto poll_result = iter_future->poll();
     while (poll_result.status() == PollStatus::Pending) {
         counter++;
         poll_result = iter_future->poll();
     }
-    std::cout << "counter = " << counter << std::endl;
-    std::cout << "poll_result = " << poll_result.get() << std::endl;
+    EXPECT_EQ((To - 1 - From) / Diff + 1, poll_result.get()); // continue times
+    EXPECT_EQ(To - From - 1 - poll_result.get(), counter); //pending times = total times - continue times
+}
+
+TEST(RangeStreamTest, StreamIter) {
+    rang_stream_to_iter<0, 20, 2>();
+    rang_stream_to_iter<1, 20, 2>();
+    rang_stream_to_iter<1, 20, 3>();
+    rang_stream_to_iter<3, 200, 7>();
 }
