e9c25b1761cbf1c4dcc5b97c04f6ed3bd1573bf2
Hexilee <hexileee@gmail.com>
2019-07-11 23:52:47 +0800

    add TODO: block event loop

--- sio/sio.cpp
+++ sio/sio.cpp
@@ -6,6 +6,7 @@
 using std::make_unique;
 thread_local auto sio::EventLoop = uvw::Loop::create();
 
+// TODO: block event loop
 auto sio::init_event_loop() -> std::unique_ptr<thread> {
     return make_unique<thread>([event_loop = sio::EventLoop]() {
         event_loop->run();
